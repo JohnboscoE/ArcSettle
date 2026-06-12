5743ba6e0e9c185227146f3f72c834f53b343889
Johnbosco <johnboscoamaobi@gmail.com>
2026-06-12 17:31:41 +0100

    fix vite reference

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly [key: string]: string | boolean | undefined
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+declare module '*.css' {
+  const content: Record<string, string>
+  export default content
+}
